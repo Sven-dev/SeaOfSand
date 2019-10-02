70ed0712ed553f54f2d427a56fbf8f5f1fe915ca
Sven Janssen <svenjanssen12321@gmail.com>
2019-10-02 16:42:15 +0200

    Improved cursor on block place, added undo functionality

--- game/Assets/Scripts/BlockPlacer.cs
+++ game/Assets/Scripts/BlockPlacer.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BlockPlacer : MonoBehaviour {
 
     public Block Prefab;
+    public Image temp;
 
     private float Yaxis;
     private List<List<Block>> UndoList;
@@ -27,6 +29,10 @@ public class BlockPlacer : MonoBehaviour {
                 //Check if a block is hit
                 if (hit.collider.tag == "CubeEdge")
                 {
+                    //Make a new list of blocks to remove
+                    List<Block> blocks = new List<Block>();
+                    UndoList.Add(blocks);
+
                     //Place a new block
                     Block b = PlaceBlock(hit.collider.transform.position);
 
@@ -34,21 +40,86 @@ public class BlockPlacer : MonoBehaviour {
                     Yaxis = b.transform.position.y;
 
                     //Place cursor on top of the placed cube
-                    //Transform grabber = b.transform.GetChild(0);              
-                    //Vector3 screenPos = Camera.main.ScreenToViewportPoint(grabber.position);        
-                    //transform.localPosition = screenPos;
+                    Transform grabber = b.transform.GetChild(0);   
+                    Vector3 screenPos = Camera.main.WorldToScreenPoint(grabber.position);
+                    screenPos.z = 0;
+                    RectTransform rect = GetComponent<RectTransform>();
+                    rect.anchoredPosition = screenPos;
 
+                    //Start multiplace
                     StartCoroutine(_Place());
                 }
             }         
         }
-	}
+
+        //Undo the last action if the B button is pressed & a isn't
+        else if (Joycons.Right.GetButtonDown(Joycon.Button.DPAD_DOWN) && !Joycons.Right.GetButton(Joycon.Button.DPAD_RIGHT))
+        {
+            Undo();
+        }
+    }
+
+    //Remove the contents of the last action (list)
+    private void Undo()
+    {
+        StartCoroutine(_Undo());
+    }
+
+    private Block PlaceBlock(Vector3 position)
+    {
+        Block b = Instantiate(Prefab, position, Quaternion.identity);
+        UndoList[UndoList.Count-1].Add(b);
+        return b;
+    }
+
+    IEnumerator _Undo()
+    {
+        float timer = 0f;
+        int SlowTimes = 3;
+        while(Joycons.Right.GetButton(Joycon.Button.DPAD_DOWN))
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                //Remove the contents of the last action (list)
+                if (UndoList.Count > 0)
+                {
+                    foreach (Block b in UndoList[UndoList.Count - 1])
+                    {
+                        Destroy(b.gameObject);
+                    }
+
+                    UndoList.RemoveAt(UndoList.Count - 1);
+                }
+
+                if (SlowTimes > 0)
+                {
+                    SlowTimes--;
+                    timer = 0.25f;
+                }
+                else
+                {
+                    timer = 0.1f;
+                }
+            }
+
+            yield return null;
+        }
+    }
 
     IEnumerator _Place()
     {
         yield return new WaitForSeconds(0.25f);
         while (Joycons.Right.GetButton(Joycon.Button.DPAD_RIGHT))
         {
+            if (Joycons.Right.GetButtonDown(Joycon.Button.DPAD_DOWN))
+            {
+                print(UndoList[UndoList.Count - 1][0].name);
+            }
+
             RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
             {
@@ -60,11 +131,5 @@ public class BlockPlacer : MonoBehaviour {
 
             yield return null;
         }
-    }
-
-    private Block PlaceBlock(Vector3 position)
-    {
-        Block b = Instantiate(Prefab, position, Quaternion.identity);
-        return b;
     }
 }
