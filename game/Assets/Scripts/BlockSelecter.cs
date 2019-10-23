2f7482d7764a73d60773b9fbef8d2a74a1434958
Sven Janssen <svenjanssen12321@gmail.com>
2019-10-23 10:22:20 +0200

    Made a prototype where you can "draw"

---
+++ game/Assets/Scripts/BlockSelecter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Probably temp, used for prototypes
+public class BlockSelecter : MonoBehaviour {
+
+    public Block Prefab;
+    public BlockPlacer BlockPlacer;
+
+    public void ChangeBlock()
+    {
+        BlockPlacer.Prefab = Prefab;
+    }
+}
